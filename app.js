eb0e1d509327f40f25e22a283e44d62a3ec86384
fawriid <farid.miri.1393@gmail.com>
2022-01-01 23:29:18 +0330

    search part is done in js, min width and height and a message is fixed for first load

--- app.js
+++ app.js
@@ -7,25 +7,46 @@ async function getData(user) {
     const resp = await fetch(APIURL + user)
     const respData = await resp.json()
     
-    getUser(respData)
+    // getUser(respData)
     return respData
 }
-getData("lorin")
+
+
 function getUser(data) {
     card.innerHTML = ""
-    card.innerHTML = `
-    <div class="img__wrapper">
-                <img src="${data.avatar_url}" alt="">
-            </div>
-            <div class="info__wrapper">
-                <h3>${data.login}</h3>
-                <span>${data.name}</span>
-                <p>${data.bio}</p>
-                <div class="social">
-                    <p>followers: <span>${data.followers}</span></p>
-                    <p>following: <span>${data.following}</span></p>
-                    <p>public_repos: <span>${data.public_repos}</span></p>
+    if (!(data.login)) {
+        card.innerHTML = `
+        <h1>User not found</h1>
+        `
+        
+    }
+     else {
+        card.innerHTML = `
+        <div class="img__wrapper">
+                    <img src="${data.avatar_url}" alt="${data.login}picture">
                 </div>
-            </div>
-    `;
+                <div class="info__wrapper">
+                    <h3>${data.login}</h3>
+                    <span>${data.name}</span>
+                    <p>${data.bio}</p>
+                    <div class="social">
+                        <p>followers: <span>${data.followers}</span></p>
+                        <p>following: <span>${data.following}</span></p>
+                        <p>public_repos: <span>${data.public_repos}</span></p>
+                    </div>
+                </div>
+        `;
+    }
 }
+
+
+
+const form = document.getElementById("search__form")
+const search = document.getElementById("search")
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const data = await getData(search.value) 
+    getUser(data)
+    search.value = ""
+})
